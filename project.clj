dc3a8c9c7aacc85ee18acb7f6220d3add119b013
James Conroy-Finn <james@logi.cl>
2015-01-28 18:07:49 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clover/lein-template "0.1.0-SNAPSHOT"
+(defproject clover/lein-template "0.1.0"
   :description "A Leiningen template for an instantly deployable Reagent web app"
   :url "https://github.com/listora/clover"
   :license {:name "The MIT License"

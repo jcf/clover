984af77206e755d2253f1869ea48e84dbb57276a
James Conroy-Finn <james@logi.cl>
2015-01-28 18:06:35 +0000

    Add clojars to deploy repositories

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/listora/clover"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
+  :deploy-repositories [["releases" :clojars]]
   :eval-in-leiningen true
   :profiles {:dev {:dependencies [[me.raynes/fs "1.4.6"]]}})
